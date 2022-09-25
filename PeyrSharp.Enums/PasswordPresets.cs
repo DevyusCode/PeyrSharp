7e37ececd4f0da4c4220de739ee646e045d790f8
Léo Peyronnet <leo.peyronnet@outlook.fr>
2022-09-25 17:36:16 +0200

    Added `PasswordPresets` enumeration

---
+++ PeyrSharp.Enums/PasswordPresets.cs
@@ -0,0 +1,41 @@
+﻿/*
+MIT License
+
+Copyright (c) Léo Corporation
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE. 
+*/
+
+namespace PeyrSharp.Enums;
+
+/// <summary>
+/// Presets that can be used for password generation.
+/// </summary>
+public enum PasswordPresets
+{
+	/// <summary>
+	/// The "Simple" preset generates a password with simple characters.
+	/// </summary>
+	Simple,
+
+	/// <summary>
+	/// The "Complex" preset generates a password with unusual, hard and complex characters.
+	/// </summary>
+	Complex
+}
