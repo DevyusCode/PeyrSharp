a8361ca00652a4fc939b2c2806ef125741295bda
Léo Peyronnet <leo.peyronnet@outlook.fr>
2022-10-25 13:53:00 +0200

    Added `GetStatusDescriptionAsync()` method (#17)

--- PeyrSharp.Core/Internet.cs
+++ PeyrSharp.Core/Internet.cs
@@ -5,3 +5,10 @@
 		/// <returns>The status code of the specified URL.</returns>
 		public static async Task<int> GetStatusCodeAsync(string url) => (int)(await new HttpClient().GetAsync(url)).StatusCode;
 
+		/// <summary>
+		/// Gets the status description of a specific page by making a request to it.
+		/// </summary>
+		/// <param name="url">The URL where to send the request.</param>
+		/// <returns>The status description of the specified URL.</returns>
+		public static async Task<string> GetStatusDescriptionAsync(string url) => (await new HttpClient().GetAsync(url)).ReasonPhrase;
+
