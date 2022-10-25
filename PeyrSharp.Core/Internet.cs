1b3045fa6a0b40d44a16307e715f9d1c70ec7f2f
Léo Peyronnet <leo.peyronnet@outlook.fr>
2022-10-25 13:53:19 +0200

    Added `IsAvailableAsync()` method (#17)

--- PeyrSharp.Core/Internet.cs
+++ PeyrSharp.Core/Internet.cs
@@ -12,3 +12,16 @@
 		/// <returns>The status description of the specified URL.</returns>
 		public static async Task<string> GetStatusDescriptionAsync(string url) => (await new HttpClient().GetAsync(url)).ReasonPhrase;
 
+		/// <summary>
+		/// Checks if a connection to the Internet is available.
+		/// </summary>
+		/// <param name="url">The page where to check if Internet is available.</param>
+		/// <returns><see langword="true"/> if the Internet can be reach.</returns>
+		public static async Task<bool> IsAvailableAsync(string url) => await GetStatusCodeAsync(url) != 400;
+
+		/// <summary>
+		/// Checks if a connection to the Internet is available.
+		/// </summary>
+		/// <returns><see langword="true"/> if the Internet can be reach.</returns>
+		public static async Task<bool> IsAvailableAsync() => await GetStatusCodeAsync("https://www.bing.com") != 400;
+
