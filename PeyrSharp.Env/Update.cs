7b04054099e72cba983b7a69fff9ccacea11f036
Léo Peyronnet <leo.peyronnet@outlook.fr>
2022-10-30 11:51:35 +0100

    Added `GetLastVersionAsync()` method (#22)

---
+++ PeyrSharp.Env/Update.cs
@@ -0,0 +1,51 @@
+﻿/*
+MIT License
+
+Copyright (c) Léo Corporation
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE. 
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PeyrSharp.Env
+{
+	/// <summary>
+	/// Class containing useful methods for implementing a simple update system.
+	/// </summary>
+	public static class Update
+	{
+		/// <summary>
+		/// Gets the lastest version of the software from a .txt file stored on a server asynchronously.
+		/// </summary>
+		/// <param name="url">Link of the file where the lastest version is stocked.</param>
+		/// <exception cref="ArgumentNullException"></exception>
+		/// <exception cref="Exception"></exception>
+		/// <returns>The content of the file stored on the specified URL.</returns>
+		public static Task<string> GetLastVersionAsync(string url)
+		{
+			HttpClient httpClient = new();
+			return httpClient.GetStringAsync(url);
+		}
+	}
+}
