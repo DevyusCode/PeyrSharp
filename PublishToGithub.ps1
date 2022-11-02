4437bbb8839ef72f5fffe7d14a9592dc49b689ba
Léo Peyronnet <leo.peyronnet@outlook.fr>
2022-11-02 11:10:13 +0100

    Updated Publish script

--- PublishToGithub.ps1
+++ PublishToGithub.ps1
@@ -9,6 +9,9 @@ function Publish {
     nuget push ".\PeyrSharp.Exceptions\bin\Release\PeyrSharp.Exceptions.$version.nupkg" -Source "github"
     nuget push ".\PeyrSharp.Enums\bin\Release\PeyrSharp.Enums.$version.nupkg" -Source "github"
     nuget push ".\PeyrSharp.UiHelpers\bin\Release\PeyrSharp.UiHelpers.$version.nupkg" -Source "github"
+    nuget push ".\PeyrSharp.Env\bin\Release\PeyrSharp.Env.$version.nupkg" -Source "github"
+    nuget push ".\PeyrSharp.Extensions\bin\Release\PeyrSharp.Extensions.$version.nupkg" -Source "github"
+    nuget push ".\PeyrSharp\bin\Release\PeyrSharp.$version.nupkg" -Source "github"
 }
 
 Publish
