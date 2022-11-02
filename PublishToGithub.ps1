7750e6f158efee8e5887eb92fab543014abdb4ec
Léo Peyronnet <leo.peyronnet@outlook.fr>
2022-11-02 11:24:54 +0100

    Updated order

--- PublishToGithub.ps1
+++ PublishToGithub.ps1
@@ -5,13 +5,13 @@ function Publish {
         [string] $version
     )
 
-    nuget push ".\PeyrSharp.Core\bin\Release\PeyrSharp.Core.$version.nupkg" -Source "github"
-    nuget push ".\PeyrSharp.Exceptions\bin\Release\PeyrSharp.Exceptions.$version.nupkg" -Source "github"
-    nuget push ".\PeyrSharp.Enums\bin\Release\PeyrSharp.Enums.$version.nupkg" -Source "github"
-    nuget push ".\PeyrSharp.UiHelpers\bin\Release\PeyrSharp.UiHelpers.$version.nupkg" -Source "github"
-    nuget push ".\PeyrSharp.Env\bin\Release\PeyrSharp.Env.$version.nupkg" -Source "github"
-    nuget push ".\PeyrSharp.Extensions\bin\Release\PeyrSharp.Extensions.$version.nupkg" -Source "github"
     nuget push ".\PeyrSharp\bin\Release\PeyrSharp.$version.nupkg" -Source "github"
+    nuget push ".\PeyrSharp.Core\bin\Release\PeyrSharp.Core.$version.nupkg" -Source "github"
+    nuget push ".\PeyrSharp.Enums\bin\Release\PeyrSharp.Enums.$version.nupkg" -Source "github"
+    nuget push ".\PeyrSharp.Env\bin\Release\PeyrSharp.Env.$version.nupkg" -Source "github"
+    nuget push ".\PeyrSharp.Exceptions\bin\Release\PeyrSharp.Exceptions.$version.nupkg" -Source "github"
+    nuget push ".\PeyrSharp.Extensions\bin\Release\PeyrSharp.Extensions.$version.nupkg" -Source "github"
+    nuget push ".\PeyrSharp.UiHelpers\bin\Release\PeyrSharp.UiHelpers.$version.nupkg" -Source "github"
 }
 
 Publish
