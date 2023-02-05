c17b297fa8fb45f8f0869dd13fed579c69c859e4
Léo Peyronnet <leo.peyronnet@outlook.fr>
2023-02-05 10:08:10 +0100

    Version 1.3.0.2302-rc1

--- PeyrSharp/PeyrSharp.cs
+++ PeyrSharp/PeyrSharp.cs
@@ -32,6 +32,6 @@ namespace PeyrSharp
 		/// <summary>
 		/// The current version of PeyrSharp.
 		/// </summary>
-		public static string Version => "1.3.0.2302-pre1";
+		public static string Version => "1.3.0.2302-rc1";
 	}
 }
