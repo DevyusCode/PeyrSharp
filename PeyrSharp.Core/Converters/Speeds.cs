0ed93bd4bf7557d5fc257964c2e3039e67fe4af8
Léo Peyronnet <leo.peyronnet@outlook.fr>
2023-05-06 08:24:12 +0200

    Added Devyus references

--- PeyrSharp.Core/Converters/Speeds.cs
+++ PeyrSharp.Core/Converters/Speeds.cs
@@ -1,7 +1,7 @@
 ﻿/*
 MIT License
 
-Copyright (c) Léo Corporation
+Copyright (c) Devyus
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -21,12 +21,6 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE. 
 */
-
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PeyrSharp.Core.Converters
 {
--- PeyrSharp.Core/Maths/Stats.cs
+++ PeyrSharp.Core/Maths/Stats.cs
@@ -1,7 +1,7 @@
 ﻿/*
 MIT License
 
-Copyright (c) Léo Corporation
+Copyright (c) Devyus
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -25,8 +25,6 @@ SOFTWARE.
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PeyrSharp.Core.Maths
 {
