29260edc4f6bbc3b1704c24e13efc42cb4fab463
Léo Peyronnet <leo.peyronnet@outlook.fr>
2023-07-29 10:22:03 +0200

    Code cleanup

--- PeyrSharp.Core/XmlHelper.cs
+++ PeyrSharp.Core/XmlHelper.cs
@@ -23,11 +23,7 @@ SOFTWARE.
 */
 
 using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Xml.Serialization;
 
 namespace PeyrSharp.Core
