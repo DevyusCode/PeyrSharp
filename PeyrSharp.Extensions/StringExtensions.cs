a9ac640202d70162623d573c5cd01daabbc1193d
Léo Peyronnet <leo.peyronnet@outlook.fr>
2023-08-30 21:05:54 +0200

    Added ToLowerAt(s) (#136)

--- PeyrSharp.Extensions/StringExtensions.cs
+++ PeyrSharp.Extensions/StringExtensions.cs
@@ -185,6 +185,13 @@ namespace PeyrSharp.Extensions
 		public static string ToUpperAt(this string s) => string.IsNullOrEmpty(s) ? s : char.ToUpper(s[0]) + s[1..];
 
 		/// <summary>
+		/// Uppercases the first letter of a string.
+		/// </summary>
+		/// <param name="s">The string to capitalize.</param>
+		/// <returns>The input string with the first letter uppercased.</returns>
+		public static string ToLowerAt(this string s) => string.IsNullOrEmpty(s) ? s : char.ToLower(s[0]) + s[1..];
+
+		/// <summary>
 		/// Reverses a <see cref="string"/>.
 		/// </summary>
 		/// <param name="input">The string to reverse.</param>
